118f7125c435f85e6696295aab53c143f7628b1f
atyutin <tutin.andrey@gmail.com>
2026-02-09 00:58:35 +0300

    hw01-solution

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,51 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.util.List;
+
+import static ru.otus.hw.utils.FileUtils.getResourceInputStream;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
+    private static final int SKIP_LINES = 1;
+
+    private static final char COMMA = ';';
+
+    private static final String QUESTION_READ_ERROR_MESSAGE = "Error reading data from file: %s";
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        List<Question> questions;
+        try (var inputFile = new InputStreamReader(getResourceInputStream(fileNameProvider.getTestFileName()), UTF_8)) {
+            var questionDtoList = new CsvToBeanBuilder<QuestionDto>(inputFile)
+                .withType(QuestionDto.class)
+                .withSkipLines(SKIP_LINES)
+                .withSeparator(COMMA)
+                .withIgnoreLeadingWhiteSpace(true)
+                .build()
+                .parse();
 
-        return new ArrayList<>();
+            questions = questionDtoList.stream()
+                .map(QuestionDto::toDomainObject)
+                .toList();
+        } catch (IOException e) {
+            throw new QuestionReadException(format(QUESTION_READ_ERROR_MESSAGE, fileNameProvider.getTestFileName()), e);
+        }
+        return questions;
     }
 }
--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,58 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+
+import static java.lang.System.lineSeparator;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.SPACE;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    private static final String QUESTIONS_NOT_FOUND_MESSAGE = "Questions not found";
+
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
-        ioService.printLine("");
+        ioService.printLine(EMPTY);
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+        var questions = questionDao.findAll();
+        if (isNotEmpty(questions)) {
+            ioService.printLine(questionsMessage(questions));
+        } else {
+            ioService.printLine(QUESTIONS_NOT_FOUND_MESSAGE);
+        }
+    }
+
+    private static String questionsMessage(List<Question> questions) {
+        StringBuilder sb = new StringBuilder();
+        int indexQuestion = 1;
+        for (Question question : questions) {
+            sb.append(indexQuestion++)
+                .append(". ")
+                .append(question.text())
+                .append(lineSeparator());
+            int indexAnswer = 1;
+            var answers = question.answers();
+            for (Answer answer : answers) {
+                sb
+                    .append(SPACE)
+                    .append(indexAnswer++)
+                    .append(") ")
+                    .append(answer.text())
+                    .append(lineSeparator());
+            }
+            sb.append(lineSeparator());
+        }
+        return sb.toString();
     }
 }
